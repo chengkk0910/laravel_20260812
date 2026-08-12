5ed62e69a786f200c6cf2b8b7fdf56e3ff19330a
kai <cgk0910@gmail.com>
2026-08-12 09:17:32 +0800

    laravel 0812 init

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController;
+
+Route::resource('cars', CarController::class);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
